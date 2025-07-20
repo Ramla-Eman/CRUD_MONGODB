7c3cf6fb46de39fd49b2fcef319c87c11f3e8155
Ramla Eman <ramla.eman.ra@gmail.com>
2025-07-20 20:10:43 +0500

    added the vercel.json

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 // Database Connection
 export const connectDB = () => {
-  mongoose.connect(process.env.MONGO_URL)
+  mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("Database Connected."))
 }
 
