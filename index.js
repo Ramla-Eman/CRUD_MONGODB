b9663bf12d042853507e60f0ea5c02119e2c2218
Ramla Eman <ramla.eman.ra@gmail.com>
2025-07-20 20:43:14 +0500

    deploying the rpoejct

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from "express"
 import dotenv from "dotenv"
-dotenv.config()
-const app = express()
+import path from "path"
 import ContactRoutes from "./routes/contacts.routes.js"
+import { fileURLToPath } from 'url';
 import { connectDB } from "./config/database.js"
 
+dotenv.config()
+const app = express()
 const PORT = process.env.PORT || 5000
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Database Connection
 connectDB()
 
 // Middleware
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
 
